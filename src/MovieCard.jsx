14ab296082b60e0f854376f784c57149268f3dfc
Your Name <okunnukazeem466@gmail.com>
2025-08-28 16:01:48 +0100

    first commit

---
+++ src/MovieCard.jsx
@@ -0,0 +1,23 @@
+
+
+
+import React from "react";
+
+const MovieCard=({ movie })=>{
+    return(
+<div className='movie'>
+          <div>
+            <p>{movie.Year}</p>
+          </div>
+          <div>
+
+            <img src={movie.Poster !== 'N/A' ? movie.Poster : "https://raw.githubusercontent.com/antonshell/placeholder-service/master/resources/test_images/img_height=400.png"} alt={movie.Title} />
+          </div>
+          <div>
+            <span>{movie.Type}</span>
+            <h3>{movie.Title}</h3>
+          </div>
+        </div>
+    )
+}
+export default MovieCard
